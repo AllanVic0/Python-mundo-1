443a8c54c3548d8b5c016c549a5ce0ec1cbccdc4
Allan Victor <limaallan260@gmail.com>
2025-01-22 15:33:25 -0300

    aula 8 e desafios

---
+++ ex016.py
@@ -0,0 +1,4 @@
+from math import trunc
+num = float(input('Digite um numero qualquer para saber sua porção inteira: '))
+numint = trunc(num)
+print('Essa aqui é a porção inteira do seu número: {}'.format(numint))
