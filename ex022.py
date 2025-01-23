5dd9efad810e02a2273d0327099168c46eede08a
Allan Victor <limaallan260@gmail.com>
2025-01-23 17:54:26 -0300

    23.01.25

---
+++ ex022.py
@@ -0,0 +1,16 @@
+nome = str(input('Digite aqui o seu nome completo: '))
+nomeM = nome.upper()
+nomem = nome.lower()
+nomeqtd = len(nome)
+qtdcaractere = len(nome.replace(" ", ""))
+qtnome1 = len(nome[:5])
+qtnome11 = len(nome.split([0]))
+
+# faltando concluir esse desafio
+
+print(nome)
+print(nomeM)
+print(nomem)
+print(qtdcaractere)
+print(qtnome1)
+print(qtnome11)
